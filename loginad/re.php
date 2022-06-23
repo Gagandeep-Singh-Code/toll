744a68c2f4d81d4396a3d797adcb87a3b98bef5d
Gagandeep Singh Dhindsa <73172200+Gagandeep-Singh-Code@users.noreply.github.com>
2022-06-23 15:26:49 +0530

    Add files via upload

---
+++ loginad/re.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+	<link rel = "stylesheet" href="login.css">
+</head>
+<body>
+<div style="background-color: #242424;" class="icon">
+<a href="http://localhost/home_page/" title="Back Button" target="_blank"><img src="http://www.downloadclipart.net/medium/19185-back-button-clip-art.png" style="height:40px;"></a>
+<!--<h1> Toll Report</h1>-->
+</div>
+<iframe height="100%" width="100%  " src="https://datastudio.google.com/embed/reporting/9185e359-36f0-4b8c-bfba-91a081a03907/page/yQLtC" frameborder="0" style="border:0" allowfullscreen></iframe>
+</body>
+</html>
